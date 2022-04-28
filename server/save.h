908f8731b4a73a07a1aa909d2ac07665a9e1dd9b
Tammy Lam <tammylam0@gmail.com>
2022-04-28 00:42:18 -0500

    Update save.h

--- server/save.h
+++ server/save.h
@@ -5,20 +5,14 @@
 
 namespace fs = std::filesystem;
 
-int
-save_ct(std::string out_fp, Ciphertext ct_result) {
-    std::cout << "Writing to " << out_fp << std::endl;
-    fs::create_directories(fs::path(out_fp).parent_path());
-    if (!fs::is_directory(fs::path(out_fp).parent_path()) || !fs::exists(fs::path(out_fp).parent_path())) { // Check if src folder exists
-        std::cout << "Creating " << fs::path(out_fp).parent_path() << std::endl;
-        fs::create_directory(fs::path(out_fp).parent_path()); // create src folder
-    }
+inline void
+save_ct(std::string fp, Ciphertext ct) {
+    std::cout << "Writing to " << fp << std::endl;
+    fs::create_directories(fs::path(fp).parent_path());
 
     std::filebuf fb;
     std::ostream os(&fb);
-    fb.open(fs::path(out_fp), std::ios::out);
-    ct_result.save(os);
+    fb.open(fs::path(fp), std::ios::out);
+    ct.save(os);
     fb.close();
-
-    return 1;
 }
